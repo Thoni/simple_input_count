0ab21883299a729c92f2f9b9d33593dfda61b4b8
Thoni <github@thonak.de>
2018-10-02 19:48:36 +0200

    Update tl_settings.php

--- system/modules/simple_input_count/languages/de/tl_settings.php
+++ system/modules/simple_input_count/languages/de/tl_settings.php
@@ -26,4 +26,4 @@ $GLOBALS['TL_LANG']['tl_settings']['simple_input_count_data'][1] = 'Geben Sie pr
 
 
 
-$GLOBALS['TL_LANG']['tl_settings']['sic-help'] = "Geben Sie pro Feld eine Zeile ein, bestehend aus:  FELDNAME,MINIMALLAENGE,MAXIMALLAENGE<br><br>Beispiel:<br>ctrl_pageTitle,55,60<br>ctrl_description,150,155"
+$GLOBALS['TL_LANG']['tl_settings']['sic-help'] = "Geben Sie pro Feld eine Zeile ein, bestehend aus:  FELDNAME,MINIMALLAENGE,MAXIMALLAENGE<br><br>Beispiel:<br>ctrl_pageTitle,55,60<br>ctrl_description,150,155";
