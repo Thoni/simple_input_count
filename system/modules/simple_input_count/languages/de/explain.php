ce4acf62411306cf7a6e2533ba0a5f0fafbdde9b
Thoni <github@thonak.de>
2018-10-19 12:10:26 +0200

    Create explain.php

---
+++ system/modules/simple_input_count/languages/de/explain.php
@@ -0,0 +1,4 @@
+<?php
+
+
+$GLOBALS['TL_LANG']['XPL']['sic-help'] = "Geben Sie pro Feld eine Zeile ein, bestehend aus:  FELDNAME,MINIMALLAENGE,MAXIMALLAENGE<br><br>Beispiel:<br>ctrl_pageTitle,55,60<br>ctrl_description,150,155";
