d1c6e34d77ef037eaf6f313e9ba2b551bf7fdbcc
Thoni <github@thonak.de>
2018-10-19 12:09:31 +0200

    Update tl_settings.php

--- system/modules/simple_input_count/languages/de/tl_settings.php
+++ system/modules/simple_input_count/languages/de/tl_settings.php
@@ -25,5 +25,3 @@ $GLOBALS['TL_LANG']['tl_settings']['simple_input_count_data'][0] = 'Feldliste';
 $GLOBALS['TL_LANG']['tl_settings']['simple_input_count_data'][1] = 'Geben Sie pro Feld eine Zeile ein, bestehend aus:  FELDNAME,MINIMALLAENGE,MAXIMALLAENGE';
 
 
-
-$GLOBALS['TL_LANG']['tl_settings']['sic-help'] = "Geben Sie pro Feld eine Zeile ein, bestehend aus:  FELDNAME,MINIMALLAENGE,MAXIMALLAENGE<br><br>Beispiel:<br>ctrl_pageTitle,55,60<br>ctrl_description,150,155";
